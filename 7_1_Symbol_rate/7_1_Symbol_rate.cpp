13de8769a1b9365f609f575044cf2d4e2232075d
Vitalii Shirinkin <artoiv2@mail.ru>
2023-08-29 21:47:49 +0300

    first commit

---
+++ 7_1_Symbol_rate/7_1_Symbol_rate.cpp
@@ -0,0 +1,39 @@
+﻿// 7_1_Symbol_rate.cpp: Задача 1. Частота символов
+//Тема: Библиотека STL
+
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
+
+void print_result(const std::vector<std::pair<char, int>>& vec) {
+
+	for (const auto& [key, value] : vec) {
+		std::cout << key << ": " << value << '\n';
+	}
+}
+
+bool compare(std::pair<char, int>p1, std::pair<char, int>p2) {
+	return p1.second > p2.second;
+}
+
+int main()
+{
+	std::string str = "Hello world!!";
+	std::cout << "[IN]: " << str << '\n';
+
+	std::unordered_map<char, int> simbol_rate;
+	std::for_each(str.cbegin(), str.cend(), [&simbol_rate](auto const& c) {
+
+		std::pair<std::unordered_map<char, int>::iterator, bool> it;
+		it = simbol_rate.insert(std::pair<char, int>(c, 1));
+		if (it.second == false)++(it.first->second);
+	});
+
+	std::vector<std::pair<char, int>>result(simbol_rate.cbegin(), simbol_rate.cend());
+	std::sort(result.begin(), result.end(), compare);
+
+	std::cout << "[OUT]: " << '\n';
+	print_result(result);
+
+	return 0;
+}
---
+++ 7_2_Removing_duplicates/7_2_Removing_duplicates/7_2_Removing_duplicates.cpp
@@ -0,0 +1,42 @@
+﻿// 7_2_Removing_duplicates.cpp : Задача 2. Устранение дубликатов
+//Тема: Библиотека STL
+
+#include<iostream>
+#include<unordered_set>
+#include<algorithm>
+
+void print_result(const std::vector<int>& vec) {
+
+	for (const auto& value : vec) {
+		std::cout << value << '\n';
+	}
+}
+
+bool compare(int p1, int p2) {
+	return p1 > p2;
+}
+
+int main()
+{
+	std::cout << "[IN]: "<<'\n';
+	int n = 0;
+	std::cin >> n;
+
+	std::unordered_set<int> nums;
+
+	int tmp = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> tmp; nums.insert(tmp);
+	}
+
+	std::vector<int> result(nums.cbegin(), nums.cend());
+	std::sort(result.begin(), result.end(), compare);
+
+	std::cout << "[OUT]: " << '\n';
+	print_result(result);
+
+	return 0;
+}
+
